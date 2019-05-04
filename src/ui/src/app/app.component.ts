a3cb0f159892eef870c123638f148e6d3b8e2bb7
andband <finnhoulihan@email.com>
2019-05-04 22:27:16 +0100

    clearing app storage when user is not logged in

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { AuthService } from "./common/service/auth/auth.service";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './common/service/auth/auth.service';
+import { Router } from '@angular/router';
+
+import { AppStorage } from './common/util/app-storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(private router: Router, private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      AppStorage.clear();
+    }
   }
 
   isLoggedIn(): boolean {
