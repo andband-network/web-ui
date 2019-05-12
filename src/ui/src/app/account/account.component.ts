e8eff910ef304f461249968a0a0cbb43e9520170
andband <finnhoulihan@email.com>
2019-05-12 23:35:36 +0100

    added editing of account information

--- src/ui/src/app/account/account.component.ts
+++ src/ui/src/app/account/account.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpService } from '../common/service/http/http.service';
+import { ProgressSpinnerService } from '../common/service/progress-spinner/progress-spinner.service';
+
 @Component({
   selector: 'account',
   templateUrl: './account.component.html',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  account;
+
+  name: string;
+  editName: boolean;
+  email: string;
+  editEmail: boolean;
+  password: string;
+  editPassword: boolean;
+
+  constructor(private spinner: ProgressSpinnerService, private http: HttpService) {
+  }
 
   ngOnInit() {
+    this.spinner.show();
+    this.http.get('/accounts')
+      .subscribe(account => {
+        // @ts-ignore
+        this.account = account;
+        this.name = this.account.name;
+        this.email = this.account.email;
+        this.password = '';
+        this.spinner.hide();
+      });
+  }
+
+  updateName(): void {
+    this.editName = false;
+    const path: string = '/accounts/name/' + this.name;
+    this.http.post(path).subscribe();
+  }
+
+  updateEmail(): void {
+    this.editEmail = false;
+    const path: string = '/accounts/email/' + this.email;
+    this.http.post(path).subscribe();
+  }
+
+  updatePassword(): void {
+    this.editPassword = false;
+    if (this.password.length > 7) {
+      const path: string = '/accounts/password/' + this.password;
+      this.http.post(path).subscribe();
+    }
   }
 
 }
